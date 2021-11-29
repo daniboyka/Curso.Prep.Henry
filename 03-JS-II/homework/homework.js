4d6fd401e0c9a5eb47658fa93e2244100f56187e
Daniel <danielcaz@gmail.com>
2021-11-29 02:32:09 -0300

    sexto commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -134,12 +134,12 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-if (numero % 5 === 0 && numero % 3 === 0) {
+if (numero % 5 == 0 && numero % 3 == 0){
   return "fizzbuzz";
-} else if (numero % 5 === 0) {
+} else if (numero % 5 == 0){
   return "buzz";
-} else if (numero % 3 === 0) {
-  return "fizz";
+} else if (numero % 3 == 0){
+  return "fizz"
 }else {
   return numero;
 }
@@ -174,26 +174,42 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+if (numero / 11 <= 11){
+  return true;
+} for (var i = 1; i <= numero; i++) {}
+  
+
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor == true){
+  return "Soy verdadero";
+} else if (valor == false) {
+  return "Soy falso";
+}
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  for (var i = 0; i <=10; i++) {
+    if ( i * 6 ){}
+  }
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero.length == 3) {
+    return true;
+  } else if (numero.length !== 3){ 
+    return false;
+  }
 }
 
 function doWhile(numero) {
